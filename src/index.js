3e3f94e2af26b7a9c5cca14780395ef8406635dd
Marina <35458264+marishahv@users.noreply.github.com>
2018-02-27 01:27:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let resultArr = [];
+
+  let equationStr = equation.replace(/ /g,'');  
+  let intArr = equationStr.match(/-?\d*\.?\d+/g);
+  
+  let a = intArr[0];
+  let b = intArr[2];
+  let c = intArr[3];
+  let discriminant = Math.pow(b, 2) - (4 * a * c); 
+
+  resultArr.push(Math.round(-b + Math.sqrt(discriminant)) / (2 * a));
+  resultArr.push(Math.round(-b - Math.sqrt(discriminant)) / (2 * a));
+  resultArr.sort((a, b) => a - b);
+  
+  return resultArr;
 }
